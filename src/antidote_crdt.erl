9f684d35b15c03eb081a51e48ee970b48cfc9504
Georges Younes <georges.r.younes@gmail.com>
2016-11-14 15:14:13 +0000

    New Map implementation with fixes

--- src/antidote_crdt.erl
+++ src/antidote_crdt.erl
@@ -37,6 +37,7 @@
                 antidote_crdt_set_rw,
                 antidote_crdt_integer,
                 antidote_crdt_map_aw,
+                antidote_crdt_map_x,
                 antidote_crdt_big_counter
                ]).
 
